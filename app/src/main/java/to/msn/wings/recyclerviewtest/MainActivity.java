a4b2c3eafeb553d93e54e95c918ede8100481fe5
marjunstudio <uebujie@gmail.com>
2024-10-09 01:16:56 +0900

    Fix:Fragmentをベースにビューを作成し直し

--- app/src/main/java/to/msn/wings/recyclerviewtest/MainActivity.java
+++ app/src/main/java/to/msn/wings/recyclerviewtest/MainActivity.java
@@ -4,38 +4,56 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private MyAdapter adapter;
 
+public class MainActivity extends AppCompatActivity {
+    public MainActivity() {
+        super(R.layout.activity_main);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        List<ListItem> items = new ArrayList<>();
-        items.add(new ListItem("Item"));
-        items.add(new ListItem("Item"));
-        items.add(new ListItem("Item"));
-        items.add(new ListItem("Item"));
-        items.add(new ListItem("Item"));
-        items.add(new ListItem("Item"));
-        items.add(new ListItem("Item"));
-        items.add(new ListItem("Item"));
-        items.add(new ListItem("Item"));
-        items.add(new ListItem("Item"));
-
-
-
-        adapter = new MyAdapter(items);
-        recyclerView.setAdapter(adapter);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, MainFragment.class, null)
+                    .commit();
+        }
     }
 }
+
+//public class MainActivity extends AppCompatActivity implements MyAdapter.OnItemClickListener{
+//    private RecyclerView recyclerView;
+//    private MyAdapter adapter;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//
+//        // TODO:RecyclerViewの定義は別ファイルに切り出す！
+//        recyclerView = findViewById(R.id.recyclerView);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//
+//        List<ListItem> items = new ArrayList<>();
+//        for (int i = 1; i <= 10; i++) {
+//            items.add(new ListItem("Item " + i));
+//        }
+//
+//        adapter = new MyAdapter(items, this);
+//        recyclerView.setAdapter(adapter);
+//    }
+//
+//    @Override
+//    public void onItemClick(ListItem item) {
+//        Intent intent = new Intent(this, DetailActivity.class);
+//        intent.putExtra(DetailActivity.EXTRA_ITEM_TEXT, item.getText());
+//        startActivity(intent);
+//    }
+//}
---
+++ app/src/main/java/to/msn/wings/recyclerviewtest/MainFragment.java
@@ -0,0 +1,9 @@
+package to.msn.wings.recyclerviewtest;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+    public MainFragment() {
+        super(R.layout.activity_main);
+    }
+}
